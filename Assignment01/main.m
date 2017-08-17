5966648f11b8cd7c28722341da00755742011052
Marusia0 <lunapelota@hotmail.com>
2017-08-17 11:13:28 -0700

    Initial Commit

---
+++ Assignment01/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  Assignment01
+//
+//  Created by Marusia Ochoa Ramírez on 17/8/17.
+//  Copyright © 2017 Marusia Ochoa Ramírez. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
